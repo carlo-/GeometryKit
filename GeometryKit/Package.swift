090b8739986b04301545a0f670cbc35d1c1a30cf
Carlo Rapisarda <carlo.rapisarda@bontouch.com>
2023-07-20 17:27:53 +0200

    Make library a Swift package

---
+++ GeometryKit/Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "GeometryKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "GeometryKit",
+            targets: ["GeometryKit", "AGGeometryKit"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "GeometryKit",
+            dependencies: [
+                "AGGeometryKit"
+            ]
+        ),
+        .target(
+            name: "AGGeometryKit",
+            dependencies: [],
+            publicHeadersPath: "Public"
+        )
+    ]
+)
---
+++ GeometryKit/Sources/GeometryKit/GeometryKit.swift
@@ -0,0 +1,4 @@
+import UIKit
+import AGGeometryKit
+
+public typealias Quad = AGGeometryKit.AGKQuad
